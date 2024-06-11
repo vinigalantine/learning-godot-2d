fd1762d48ee7463b5ad1cf676a94be7aa05f5065
Vinícius L. Galantine <vinicius.galantine@outlook.com>
2024-06-11 02:45:47 -0300

    Started "Rank" functionality
    First step into the "Rank" functionality, now it asks for your initials at the game over

--- hud.gd
+++ hud.gd
@@ -20,8 +20,56 @@ func show_game_over():
 	show_message("Game Over")
 	# Wait for the MessageTimer count
 	await  $MessageTimer.timeout
+	show_score_save_hud()
 	
+func show_score_save_hud():
+	$Message.vertical_alignment = VERTICAL_ALIGNMENT_TOP
+	$Message.text = "What's your name?"
+	$Message.show()
+	
+	$NamePlayerScore.show()
+	$NamePlayerScore.grab_focus()
+	$SaveButton.show()
+	
+func save_score():
+	var usr : String = $NamePlayerScore.text
+	
+	if $Message.text == "WHAT'S YOUURRR NAME?" && (usr == "" || usr == null):
+		$Message.text = "Tony? 👀"
+	elif usr == "" || usr == null:
+		#var font = $Message.
+		$Message.add_theme_font_size_override("font_size",45)
+		$Message.text = "WHAT'S YOUURRR NAME?"
+	else:
+		var score = int($ScoreLabel.text)
+		
+		if !FileAccess.file_exists("user://rank.json"):
+			var baseRank = "{ \"rank\": [] }"
+			var firstRank = FileAccess.open("user://rank.json",FileAccess.WRITE)
+			firstRank.store_string(baseRank)
+			firstRank.close()
+		
+		var rankFile = FileAccess.open("user://rank.json", FileAccess.READ_WRITE)
+		
+		if rankFile.get_as_text() != "" || rankFile.get_as_text() != null:
+			var rankParse : Dictionary = JSON.parse_string(rankFile.get_as_text())
+			var userRank = { "user": usr, "score": score }
+			
+			rankParse["rank"].append( userRank )
+			
+			rankFile.store_string(JSON.stringify(rankParse))
+			rankFile.close()
+		
+		$NamePlayerScore.clear()
+		$NamePlayerScore.hide()
+		$SaveButton.hide()
+		show_new_game()
+
+func show_new_game():
+	$Message.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	$Message.add_theme_font_size_override("font_size",65)
 	$Message.text = "Dodge if you can!"
+	$ScoreLabel.text = "0"
 	$Message.show()
 	# Make an one-shot timer and wait for it
 	await get_tree().create_timer(1.0).timeout
@@ -37,3 +85,11 @@ func _on_start_button_pressed():
 
 func _on_mesage_timer_timeout():
 	$Message.hide()
+
+func _on_name_player_score_text_changed(new_text):
+	var caret_column = $NamePlayerScore.caret_column
+	$NamePlayerScore.text = new_text.to_upper()
+	$NamePlayerScore.caret_column = caret_column
+
+func _on_name_player_score_text_submitted(new_text):
+	save_score()
--- main.gd
+++ main.gd
@@ -53,12 +53,10 @@ func _on_mob_timer_timeout():
 	
 	#Spawn mob
 	add_child(mob)	
-
-
+ 
 func _on_score_timer_timeout():
 	score += 1
 	$HUD.update_score(score)
-
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
